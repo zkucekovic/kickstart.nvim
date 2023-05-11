fbb0578be61ebf44084c5ebe1ec38e61fe147044
Zoran Kucekovic <zoran.kucekovic@outlook.com>
2023-05-11 12:34:46 +0200

    catppuccin theme

--- init.lua
+++ init.lua
@@ -124,13 +124,10 @@ require('lazy').setup({
     },
   },
 
+  -- Catppuccin Theme
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
+    "catppuccin/nvim",
+    name = "catppuccin"
   },
 
   {
@@ -597,6 +594,54 @@ require('toggleterm').setup {
   direction = 'float',
   auto_scroll = true,
 }
+
+-- Catppuccin Colorscheme
+require("catppuccin").setup({
+  flavour = "frappe", -- latte, frappe, macchiato, mocha
+  background = {
+    -- :h background
+    light = "latte",
+    dark = "mocha",
+  },
+  transparent_background = false,
+  show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+  term_colors = false,
+  dim_inactive = {
+    enabled = false,
+    shade = "dark",
+    percentage = 0.15,
+  },
+  no_italic = false, -- Force no italic
+  no_bold = false,   -- Force no bold
+  styles = {
+    comments = { "italic" },
+    conditionals = { "italic" },
+    loops = {},
+    functions = {},
+    keywords = {},
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = {},
+    properties = {},
+    types = {},
+    operators = {},
+  },
+  color_overrides = {},
+  custom_highlights = {},
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    nvimtree = true,
+    telescope = true,
+    notify = false,
+    mini = false,
+    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+  },
+})
+
+-- setup must be called before loading
+vim.cmd.colorscheme "catppuccin"
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
